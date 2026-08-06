cb8fcc5e10cf8227d0f4fcd48ac888518bda539d
Diego Mugetsu <diegoavilafacebook75@gmail.com>
2026-08-06 10:27:46 -0600

    Updated bundled mods (added first person kanto, updated voxel mod compatible with first person kanto)

--- mods/cloud mod/main.lua
+++
@@ -1,7 +0,0 @@
-return function(mod)
-mod.content.sprites:patch("SPRITE_RED", {
-  image = mod.assets:path("assets/sonicplayer.png"),})
-mod.content.sprites:patch("SPRITE_RED_BIKE", {
-  image = mod.assets:path("assets/sonicplayer_bike.png"),
-})
-end
